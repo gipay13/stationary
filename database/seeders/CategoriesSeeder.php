ef3759b9478183c72bc6fa48b8607c44cbac6a78
Ghifary Anshar <ghifariansarariestiand@gmail.com>
2024-04-20 22:28:38 +0700

    update: kategori seeder

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -19,28 +19,24 @@ class CategoriesSeeder extends Seeder
                 [
                     'id' => 1,
                     'nama' => 'Elektronik',
-                    'slug' => Str::slug('Elektronik'),
                     'created_at' => now(),
                     'updated_at' => now(),
                 ],
                 [
                     'id' => 2,
                     'nama' => 'Alat Tulis Kantor',
-                    'slug' => Str::slug('Alat Tulis Kantor'),
                     'created_at' => now(),
                     'updated_at' => now(),
                 ],
                 [
                     'id' => 3,
                     'nama' => 'Perabotan',
-                    'slug' => Str::slug('Perabotan'),
                     'created_at' => now(),
                     'updated_at' => now(),
                 ],
                 [
                     'id' => 4,
                     'nama' => 'Alat Kebersihan',
-                    'slug' => Str::slug('Alat Kebersihan'),
                     'created_at' => now(),
                     'updated_at' => now(),
                 ],
