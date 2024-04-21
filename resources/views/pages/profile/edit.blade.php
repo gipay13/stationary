8ee861e1d2707cca51c27b3574d1393f516ea9a8
Ghifary Anshar <ghifariansarariestiand@gmail.com>
2024-04-21 11:33:33 +0700

    update: hapus fungsi delete akun di profile

--- resources/views/pages/profile/edit.blade.php
+++ resources/views/pages/profile/edit.blade.php
@@ -22,12 +22,6 @@
                     @include('pages.profile.partials.update-password-form')
                 </div>
             </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('pages.profile.partials.delete-user-form')
-                </div>
-            </div>
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::middleware(['role:Admin'])->group(function () {
         // Category
